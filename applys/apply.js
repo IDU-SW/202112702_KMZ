17e15fb14a9a6c3823bd3cbcee7a72b8582e56d3
min-zi <min-zi@naver.com>
2021-06-22 17:21:51 +0900

    applys update

--- applys/apply.js
+++ applys/apply.js
@@ -1,71 +1,66 @@
 const Apply = require("../models/Apply.js");
 
-async function add(req, res) {
-  try {
-    const { product, price, manager, area } = req.body;
-    await Apply.create({
-      product: product,
-      price: price,
-      manager: manager,
-      area: area,
-    });
-    res.json("Apply added success!!!");
-  } catch {
-    res.status(404).send("---not found---");
-  }
-}
-
-async function list(req, res) {
-  try {
-    const list = await Apply.findAll({});
-    res.json(list);
-  } catch (err) {
-    console.log(err);
-    res.status(404).send("---not found---");
-  }
-}
-
-async function complete(req, res) {
-  const { id, product, price, manager, area } = req.body;
-
-  try {
-    const TodoExist = await Apply.findOne({ where: { id: id }, raw: true });
-    if (!TodoExist) {
-      res.status(400).send("There is not Apply");
-      return false;
-    }
-
-    await Apply.update(
-      {
+export default {
+  add: async (req, res) => {
+    try {
+      const { product, price, manager, area } = req.body;
+      await Apply.create({
         product: product,
         price: price,
         manager: manager,
         area: area,
-      },
-      { where: { id: id } }
-    ).then(() => {
-      res.status(200).json("Apply update success!!!");
-    });
-  } catch {
-    res.status(404).send("---not found---");
-  }
-}
+      });
+      res.json("Apply added success!!!");
+    } catch {
+      res.status(404).send("---not found---");
+    }
+  },
 
-async function deleteApply(req, res) {
-  const { id } = req.body;
+  list: async (req, res) => {
+    try {
+      const list = await Apply.findAll({});
+      res.json(list);
+    } catch (err) {
+      console.log(err);
+      res.status(404).send("---not found---");
+    }
+  },
 
-  try {
-    await Apply.destroy({ where: { id: id } }).then(() => {
-      res.status(200).json("Apply delete success!!!");
-    });
-  } catch {
-    res.status(404).send("---not found---");
-  }
-}
+  complete: async (req, res) => {
+    const { id, product, price, manager, area } = req.body;
 
-exports.apply = {
-  add,
-  list,
-  complete,
-  deleteApply,
+    try {
+      const TodoExist = await Apply.findOne({ where: { id: id }, raw: true });
+      if (!TodoExist) {
+        res.status(400).send("There is not Apply");
+        return false;
+      }
+
+      await Apply.update(
+        {
+          product: product,
+          price: price,
+          manager: manager,
+          area: area,
+        },
+        { where: { id: id } }
+      ).then(() => {
+        res.status(200).json("Apply update success!!!");
+      });
+    } catch {
+      res.status(404).send("---not found---");
+    }
+  },
+
+  delete: async (req, res) => {
+    const { id } = req.body;
+
+    try {
+      await Apply.destroy({ where: { id: id } }).then(() => {
+        res.status(200).json("Apply delete success!!!");
+      });
+    } catch {
+      res.status(404).send("---not found---");
+    }
+  },
 };
--- applys/index.js
+++ applys/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const { apply } = require("./apply.js");
+const apply = require("./apply.js").default;
 
 router.get("/", apply.list);
 router.post("/add", apply.add);
 router.put("/update", apply.complete);
-router.delete("/delete", apply.deleteApply);
+router.delete("/delete", apply.delete);
 
 module.exports = router;
